da27aa9f1a3622ae20f8b85b9e4015a74b80919f
vimal199 <vimalbhardwaj8@gmail.com>
2023-09-04 02:10:23 +0530

    Adding back end as in excercise 4.1-4.7

---
+++ part4/Blog_application/utils/middleware.js
@@ -0,0 +1,27 @@
+const logger = require('./logger')
+const requestLogger = (request, response, next) => {
+    logger.info('Method: ', request.method)
+    logger.info('Path: ', request.path)
+    logger.info('Body: ', request.body)
+    logger.info('---')
+    next()
+}
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown ' })
+}
+const errorHandler = (error, request, response, next) => {
+    logger.info(error.message)
+    if (error.name === 'CastError') {
+        return response.status(404).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({ error: error.message })
+    } else if (error.name === 'TokenExpiredError') {
+        return response.status(401).json({ error: 'Token expired' })
+    }
+    next(error)
+}
+module.exports = {
+    requestLogger, unknownEndpoint, errorHandler
+}
